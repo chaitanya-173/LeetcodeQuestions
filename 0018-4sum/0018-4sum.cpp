edf05155025bca68b8628a70cf616b40d06bc12e
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-03-14 23:19:47 +0530

    Time: 13 ms (91.73%), Space: 17.4 MB (85.69%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        set<vector<int>> set;
         vector<vector<int>> output;
         for(int i=0; i<n-3; i++){
             for(int j=i+1; j<n-2; j++){
@@ -17,14 +16,15 @@ public:
                         high--;
                     }
                     else{
-                        set.insert({nums[i], nums[j], nums[low], nums[high]});
-                        low++; high--;
+                        output.push_back({nums[i], nums[j], nums[low], nums[high]});
+                        int tempIndex1 = low, tempIndex2 = high;
+                        while(low < high && nums[low] == nums[tempIndex1]) low++;
+                        while(low < high && nums[high] == nums[tempIndex2]) high--;
                     }
                 }
+                while(j+1 < n && nums[j] == nums[j+1]) j++;
             }
-        }
-        for(auto it : set){
-            output.push_back(it);
+            while(i+1 < n && nums[i] == nums[i+1]) i++;
         }
         return output;
     }
