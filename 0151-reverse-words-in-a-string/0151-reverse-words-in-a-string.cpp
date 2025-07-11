6d2f36c1c05b0c6498ef3197eab94e317aedd9dc
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-07-11 07:59:33 +0530

    Time: 0 ms (100%), Space: 10.5 MB (53.75%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,20 +3,22 @@ public:
     string reverseWords(string s) {
         stringstream ss(s);
         string word;
-        vector<string> words;
+        stack<string> words;
+        string ans;
 
         while (ss >> word) {
-            words.push_back(word);
+            words.push(word);
         }
 
-        string res;
-        for (int i = words.size() - 1; i >= 0; i--) {
-            res += words[i];
-            if (i != 0) {
-                res += " ";
-            }
+        while(!words.empty()) {
+            ans += words.top();
+            words.pop();
+            if(!words.empty()) ans += " ";
         }
 
-        return res;
+
+        
+
+        return ans;
     }
 };
