6c5e1ae863b4f0202dbc9507268d0f1ee85ecb91
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-07-13 20:59:35 +0530

    Time: 0 ms (100%), Space: 8 MB (96.99%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n = s.length();
-        if(n != goal.length()) return false;
-
-        for(int i=0; i<n; i++) {
-            int temp = s[0];
-            for(int i=1; i<n; i++) {
-                s[i-1] = s[i];
-            }
-            s[n-1] = temp;
-            if(s == goal) return true;
-        }
-
-        return false;
+        return s.length() == goal.length() && (s + s).find(goal) != string::npos;
     }
 };
