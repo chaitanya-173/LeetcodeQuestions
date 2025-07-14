17bfd1eb2a5b6493b9dd422f9d554721feae9429
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-07-14 22:41:19 +0530

    Time: 15 ms (10.02%), Space: 13.4 MB (36.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> mp{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
-
+        unordered_map<char, int> mp{
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M', 1000}
+        };
+        
         int sum = 0;
-        for(int i=0; i<s.length(); i++) {
-            if( (s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) ||
-                (s[i] == 'X' && (s[i+1] == 'L' || s[i+1] == 'C')) || 
-                (s[i] == 'C' && (s[i+1] == 'D' || s[i+1] == 'M')) ) {
-                sum += (mp[s[i+1]] - mp[s[i]]);
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() &&
+                ((s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X')) ||
+                 (s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C')) ||
+                 (s[i] == 'C' && (s[i + 1] == 'D' || s[i + 1] == 'M')))) {
+                sum += (mp[s[i + 1]] - mp[s[i]]);
                 i++;
-                continue;
+            } else {
+                sum += mp[s[i]];
             }
-             
-            sum += mp[s[i]];
         }
 
         return sum;
