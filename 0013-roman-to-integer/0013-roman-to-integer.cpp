fc23f76e85b5f9d798f277e6fb1619d94dc40242
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-07-14 22:29:26 +0530

    Time: 6 ms (55.03%), Space: 13.3 MB (36.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,41 +5,12 @@ public:
 
         int sum = 0;
         for(int i=0; i<s.length(); i++) {
-            if(s[i] == 'I') {
-                if(s[i+1] == 'V') {
-                    sum += 4;
-                    i+=1;
-                    continue;
-                }
-                if(s[i+1] == 'X') {
-                    sum += 9;
-                    i+=1;
-                    continue;
-                }
-            }
-            if(s[i] == 'X') {
-                if(s[i+1] == 'L') {
-                    sum += 40;
-                    i+=1;
-                    continue;
-                }
-                if(s[i+1] == 'C') {
-                    sum += 90;
-                    i+=1;
-                    continue;
-                }
-            }
-            if(s[i] == 'C') {
-                if(s[i+1] == 'D') {
-                    sum += 400;
-                    i+=1;
-                    continue;
-                }
-                if(s[i+1] == 'M') {
-                    sum += 900;
-                    i+=1;
-                    continue;
-                }
+            if( (s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) ||
+                (s[i] == 'X' && (s[i+1] == 'L' || s[i+1] == 'C')) || 
+                (s[i] == 'C' && (s[i+1] == 'D' || s[i+1] == 'M')) ) {
+                sum += (mp[s[i+1]] - mp[s[i]]);
+                i++;
+                continue;
             }
              
             sum += mp[s[i]];
