293e8b9bc8cd18650060504201201212bc320260
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-07-16 22:56:09 +0530

    Time: 11 ms (69.13%), Space: 9.2 MB (92.92%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-
-    bool isPalindrome(string s, int i, int j) {
-        while(i <= j) {
-            if(s[i++] != s[j--]) return false;
-        }
-        return true;
-    }
-
     string longestPalindrome(string s) {
-        int n = s.length();
-        int maxi = 0;
-        int index = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=i; j<n; j++) {
-                if(s[i] == s[j]) {
-                    if(isPalindrome(s, i, j)) {
-                        if(j-i+1 > maxi) {
-                            maxi = j-i+1;
-                            index = i;
-                        }
-                    }
-                }
+        int n = s.size();
+        if (n <= 1) return s; // agar string size 0 ya 1 hai, wahi palindrome hai
+
+        int start = 0;  // best palindrome ka starting index
+        int maxLen = 1; // ab tak ka longest palindrome length
+
+        // Helper function to expand around center
+        auto expandAroundCenter = [&](int left, int right) {
+            // Jab tak left aur right valid hai aur same char hai, expand karo
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                left--;
+                right++;
             }
+            // Loop khatam hote hi, left aur right ek extra step bahar chale gaye
+            int len = right - left - 1;   // actual palindrome length
+            if (len > maxLen) {           // agar yeh maxLen se bada hai, update karo
+                maxLen = len;
+                start = left + 1;        // left+1 se start hota hai palindrome
+            }
+        };
+
+        // Har character ko center maan ke check karo
+        for (int i = 0; i < n; i++) {
+            expandAroundCenter(i, i);     // Odd length palindrome
+            expandAroundCenter(i, i + 1); // Even length palindrome
         }
-        return s.substr(index, maxi);
+
+        // Longest palindrome substring return karo
+        return s.substr(start, maxLen);
     }
 };
