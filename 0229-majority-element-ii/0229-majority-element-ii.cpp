0850cff015df5d6ade94fcdfb2fa79ee955e7123
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-07-22 22:46:56 +0530

    Time: 0 ms (100%), Space: 23.5 MB (79.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,14 +2,28 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> freq;
-        for(auto i: nums) {
-            freq[i]++;
+        int el1 = 0, el2 = 1; // initialize with any two different values
+        int cnt1 = 0, cnt2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) cnt1++;
+            else if (nums[i] == el2) cnt2++;
+            else if (cnt1 == 0) { el1 = nums[i]; cnt1 = 1; }
+            else if (cnt2 == 0) { el2 = nums[i]; cnt2 = 1; }
+            else { cnt1--; cnt2--; }
         }
+
+        // Count actual occurrences
+        cnt1 = cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) cnt1++;
+            else if (nums[i] == el2) cnt2++;
+        }
+
         vector<int> ans;
-        for(auto i: freq) {
-            if(i.second > n/3) ans.push_back(i.first);
-        }
+        int mini = n / 3 + 1;
+        if (cnt1 >= mini) ans.push_back(el1);
+        if (cnt2 >= mini) ans.push_back(el2);
         return ans;
     }
 };
