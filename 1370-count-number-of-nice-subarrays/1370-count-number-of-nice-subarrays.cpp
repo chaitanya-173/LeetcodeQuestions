1955d3e39e10fb6effcd7337daae4a374d5f7d0f
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-07-31 22:23:52 +0530

    Time: 72 ms (17.67%), Space: 94.2 MB (20.37%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixCount;
-        prefixCount[0] = 1; // base case
-
-        int count = 0, oddCount = 0;
-
-        for (int num : nums) {
-            // increase oddCount if num is odd
-            oddCount += num % 2;
-
-            // check if there exists a prefix where oddCount - k occurred
-            if (prefixCount.find(oddCount - k) != prefixCount.end()) {
-                count += prefixCount[oddCount - k];
-            }
-
-            // store current prefix count
-            prefixCount[oddCount]++;
+        unordered_map<int,int> prefixSum;
+        prefixSum[0] = 1;
+        int count = 0, sum = 0;
+        for(int i=0; i<nums.size(); i++) {
+            sum += nums[i] % 2;
+            if(prefixSum.find(sum - k) != prefixSum.end()) {
+                count += prefixSum[sum - k];
+            } 
+            prefixSum[sum]++;
         }
-
         return count;
     }
 };
