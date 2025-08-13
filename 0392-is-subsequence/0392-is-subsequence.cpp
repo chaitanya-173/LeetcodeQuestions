2a51d0219116e0d21372a195ab9a03c8890fc383
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-08-13 21:20:51 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.39%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
-                if(s[i-1] == t[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+        int i = 0;
+        for(int j=0; j<t.size(); j++) {
+            if(t[j] == s[i]) {
+                i++;
+            } 
         }
-
-        string lcs = "";
-        int i = n, j = m;
-        while(i > 0 && j > 0) {
-            if(s[i-1] == t[j-1]) {
-                lcs += s[i-1];
-                i--;
-                j--;
-            } else if(dp[i-1][j] > dp[i][j-1]) {
-                i--;
-            } else {
-                j--;
-            }
+        if(i == s.size()) {
+            return true;
         }
-
-        reverse(lcs.begin(), lcs.end());
-
-        return (lcs == s);
+        return false;
     }
 };
