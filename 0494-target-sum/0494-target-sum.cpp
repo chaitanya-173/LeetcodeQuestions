7efbaa4f2f0b0e217d43bb634f6fcca614ac949e
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-08-26 22:22:34 +0530

    Time: 0 ms (100%), Space: 15.7 MB (41.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<int, int> counter;
-        counter[0] = 1;
+        int n = nums.size();
 
-        for (int n : nums) {
-            unordered_map<int, int> temp;
-
-            for (const auto& [total, count] : counter) {
-                temp[total + n] += count;
-                temp[total - n] += count;
-            }
-            counter = temp;
+        int sumOfArr = 0;
+        for(int num : nums) {
+            sumOfArr += num;
         }
 
-        return counter[target];        
+        // Invalid cases
+        if (target > sumOfArr || target < -sumOfArr) return 0;
+        if ((target + sumOfArr) % 2 != 0) return 0;
+
+        int sum = (target + sumOfArr) / 2;
+
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
+
+        // Base case: sum = 0
+        dp[0][0] = 1;
+
+        for(int i=1; i<=n; i++) {
+            for(int j=0; j<=sum; j++) {
+                if(nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][sum];
     }
 };
