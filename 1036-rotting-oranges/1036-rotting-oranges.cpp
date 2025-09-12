57802a00c79d1c9ddaf947d84f89089ab45fa89d
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-09-12 21:26:44 +0530

    Time: 2 ms (34.34%), Space: 17.3 MB (18.7%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -5,12 +5,15 @@ public:
         int m = grid[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
         queue<pair< pair<int,int>, int >> q;
+        int cntFresh = 0;
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == 2) {
                     q.push({{i,j}, 0});
                     vis[i][j] = 2;
+                } else if(grid[i][j] == 1) {
+                    cntFresh++;
                 }
             }
         }
@@ -34,18 +37,11 @@ public:
                 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
                     q.push({{nrow, ncol}, t+1});
                     vis[nrow][ncol] = 2;
+                    cntFresh--;
                 }
             }
         }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(grid[i][j] == 1 && vis[i][j] != 2) {
-                    return -1;
-                }
-            }
-        }
-
-        return time;
+        return (cntFresh != 0 ? -1 : time);
     }
 };
