f3c90ef364872ab96746e1208e059f6e846df20f
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-10-18 22:55:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (93.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,44 +1,55 @@
-class Solution {
-public:
-    void bfs(int node, vector<vector<int>> &adj, vector<bool> &vis) {
-        queue<int> q;
-        q.push(node);
-        vis[node] = true;
+#include <vector>
+using namespace std;
 
-        while(!q.empty()) {
-            int frontNode = q.front();
-            q.pop();
-            
-            for(auto it: adj[frontNode]) {
-                if(!vis[it]) {
-                    vis[it] = true;
-                    q.push(it);
-                }
-            }
-        }
+class DisjointSet {
+    vector<int> size, parent;
+public:
+    // constructor
+    DisjointSet(int n) { 
+        size.resize(n, 1); // initially each node size = 1
+        parent.resize(n);
+        for(int i=0; i<n; i++) parent[i] = i;
     }
 
+    int findParent(int node) {
+        if(parent[node] == node) return node;
+        return parent[node] = findParent(parent[node]); // path compression
+    }
+
+    void unionBySize(int u, int v) {
+        int ulpU = findParent(u);
+        int ulpV = findParent(v);
+        if(ulpU == ulpV) return;
+
+        if(size[ulpU] < size[ulpV]) {
+            parent[ulpU] = ulpV;
+            size[ulpV] += size[ulpU];
+        } else {
+            parent[ulpV] = ulpU;
+            size[ulpU] += size[ulpV];
+        }
+    }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V = isConnected.size();
+        DisjointSet ds(V);
 
-        // making adj list
-        vector<vector<int>> adj(V);
+        // connect directly connected cities
         for(int i=0; i<V; i++) {
             for(int j=0; j<V; j++) {
-                if(isConnected[i][j] == 1 && i != j) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                if(isConnected[i][j] == 1) {
+                    ds.unionBySize(i, j);
                 }
             }
         }
 
-        vector<bool> vis(V, false);
+        // count number of unique ultimate parents
         int cnt = 0;
         for(int i=0; i<V; i++) {
-            if(!vis[i]) {
-                cnt++;
-                bfs(i, adj, vis);
-            }
+            if(ds.findParent(i) == i) cnt++;
         }
 
         return cnt;
