70c9b49555cc5a0755deeebdc2c96c3c213f9e8c
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-10-22 19:24:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (75.26%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,52 @@
+class DisjointSet {
+    vector<int> parent, size;
+    public:
+        DisjointSet(int n) {
+            parent.resize(n+1);
+            size.resize(n+1, 1);
+            for(int i=0; i<=n; i++) parent[i] = i;
+        }
+
+        int findUParent(int node) {
+            if(parent[node] == node) return node;
+            return parent[node] = findUParent(parent[node]);
+        }  
+
+        void unionBySize(int u, int v) {
+            int ulpU = findUParent(u);
+            int ulpV = findUParent(v);
+            if(ulpU == ulpV) return;
+            if(size[ulpU] < size[ulpV]) {
+                parent[ulpU] = ulpV;
+                size[ulpV] += size[ulpU];
+            } else {
+                parent[ulpV] = ulpU;
+                size[ulpU] += size[ulpV]; 
+            }
+        }
+};
+
 class Solution {
-private:
-    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
-    {
-        visited[src] = true;
-        for(int i : adj[src])
-            if(!visited[i])
-                dfs(adj, visited, i);
-    }
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        
-        if(connections.size() < n - 1)
-            return -1;
-        vector<vector<int>> adj(n);
-        for(auto v : connections)
-        {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
-        }
-        vector<bool> visited(n, false);
-        int components = 0;
-        for(int i=0; i<n; i++)
-            if(!visited[i])
-            {
-                dfs(adj, visited, i);
-                components++;
+        DisjointSet ds(n);
+        int noOfEdges = 0;
+        for(int i=0; i<connections.size(); i++) {
+            int u = connections[i][0];
+            int v = connections[i][1];
+            if(ds.findUParent(u) != ds.findUParent(v)) {
+                ds.unionBySize(u, v);
+            } else {
+                noOfEdges++;
             }
-        return components - 1;
+        }
+
+        int noOfComponents = 0;
+        for(int i=0; i<n; i++) {
+            if(ds.findUParent(i) == i) noOfComponents++;
+        }
+
+        if(noOfComponents - noOfEdges <= 1) return noOfComponents - 1;
+        return -1;
     }
 };
