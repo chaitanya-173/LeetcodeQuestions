de193896bced8e763f0adb17161d3bc5c08130a7
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-10-22 15:56:36 +0530

    Time: 4 ms (29.97%), Space: 19.5 MB (75.8%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,55 +1,47 @@
-#include <vector>
-using namespace std;
-
 class DisjointSet {
-    vector<int> size, parent;
-public:
-    // constructor
-    DisjointSet(int n) { 
-        size.resize(n, 1); // initially each node size = 1
-        parent.resize(n);
-        for(int i=0; i<n; i++) parent[i] = i;
-    }
-
-    int findParent(int node) {
-        if(parent[node] == node) return node;
-        return parent[node] = findParent(parent[node]); // path compression
-    }
-
-    void unionBySize(int u, int v) {
-        int ulpU = findParent(u);
-        int ulpV = findParent(v);
-        if(ulpU == ulpV) return;
-
-        if(size[ulpU] < size[ulpV]) {
-            parent[ulpU] = ulpV;
-            size[ulpV] += size[ulpU];
-        } else {
-            parent[ulpV] = ulpU;
-            size[ulpU] += size[ulpV];
+    vector<int> parent, size;
+    public:
+        DisjointSet(int n) {
+            parent.resize(n+1);
+            size.resize(n+1, 1);
+            for(int i=0; i<=n; i++) parent[i] = i;
         }
-    }
+
+        int findUParent(int node) {
+            if(parent[node] == node) return node;
+            return parent[node] = findUParent(parent[node]);
+        }
+
+        void unionBySize(int u, int v) {
+            int ulpU = findUParent(u);
+            int ulpV = findUParent(v);
+            if(ulpU == ulpV) return;
+            if(size[ulpU] < size[ulpV]) parent[ulpU] = ulpV;
+            else if(size[ulpU] > size[ulpV]) parent[ulpV] = ulpU;
+            else {
+                parent[ulpV] = ulpU;
+                size[ulpU] += size[ulpV];
+            }
+        }
 };
 
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V = isConnected.size();
-        DisjointSet ds(V);
+        int n = isConnected.size();
+        DisjointSet ds(n);
 
-        // connect directly connected cities
-        for(int i=0; i<V; i++) {
-            for(int j=0; j<V; j++) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
                 if(isConnected[i][j] == 1) {
                     ds.unionBySize(i, j);
                 }
             }
         }
 
-        // count number of unique ultimate parents
         int cnt = 0;
-        for(int i=0; i<V; i++) {
-            if(ds.findParent(i) == i) cnt++;
+        for(int i=0; i<n; i++) {
+            if(ds.findUParent(i) == i) cnt++;
         }
 
         return cnt;
