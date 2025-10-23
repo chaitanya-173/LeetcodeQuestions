59045b7bb592718a4f8e25895b6627d381138ccc
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-10-23 16:44:47 +0530

    Time: 3 ms (29.09%), Space: 17.4 MB (13.23%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -4,13 +4,14 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair< pair<int,int>, int >> q;
+        // queue<pair< pair<int,int>, int >> q;
+        queue<tuple<int,int,int>> q;
         int cntFresh = 0;
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == 2) {
-                    q.push({{i,j}, 0});
+                    q.push({i,j,0});
                     vis[i][j] = 2;
                 } else if(grid[i][j] == 1) {
                     cntFresh++;
@@ -23,9 +24,13 @@ public:
         int time = 0;
 
         while(!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
+            auto it = q.front();
+            int r = get<0>(it);
+            int c = get<1>(it);
+            int t = get<2>(it);
+            // int r = q.front().first.first;
+            // int c = q.front().first.second;
+            // int t = q.front().second;
             q.pop();
             time = max(time, t);
 
@@ -35,7 +40,7 @@ public:
 
                 if(nrow >= 0 && ncol >= 0 && nrow < n 
                 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
-                    q.push({{nrow, ncol}, t+1});
+                    q.push({nrow, ncol, t+1});
                     vis[nrow][ncol] = 2;
                     cntFresh--;
                 }
