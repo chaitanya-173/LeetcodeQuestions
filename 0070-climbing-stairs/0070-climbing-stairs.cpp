bf70017b25ed03b048f4ef234f8afd68e44043a4
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-11 14:59:57 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n <= 1) return 1;
-        vector<int> dp(n+1);
-        dp[0] = 1;
-        dp[1] = 1;
+        // vector<int> dp(n+1);
+        int prev = 1;
+        int prev2 = 1;
+        // dp[0] = 1;
+        // dp[1] = 1;
         for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            // dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
