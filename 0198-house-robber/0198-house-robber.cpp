95aa98f2e0fbe5fb0eb01bfb79e7a9521ac8bf4b
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-12 14:23:04 +0530

    Time: 0 ms (100%), Space: 10 MB (97.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,20 +13,36 @@ public:
     //     return dp[n] = max(prev, prev2);
     // }
 
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n == 1) return nums[0];
+
+    //     vector<int> dp(n);
+    //     dp[0] = nums[0];
+    //     dp[1] = max(nums[0], nums[1]);
+
+    //     for(int i=2; i<n; i++) {
+    //         int prev = dp[i-1];
+    //         int prev2 = dp[i-2] + nums[i];
+    //         dp[i] = max(prev, prev2);
+    //     }
+
+    //     return dp[n-1];
+    // }
+    
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
 
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev2 = nums[0];
+        int prev = max(nums[0], nums[1]);
 
         for(int i=2; i<n; i++) {
-            int prev = dp[i-1];
-            int prev2 = dp[i-2] + nums[i];
-            dp[i] = max(prev, prev2);
+            int curr = max(prev, prev2 + nums[i]);
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n-1];
+        return prev;
     }
 };
