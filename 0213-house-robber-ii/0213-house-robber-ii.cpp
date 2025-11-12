b0af617cdd97816c1fabb3fe0f1c766269730af0
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-12 15:08:12 +0530

    Time: 0 ms (100%), Space: 10.2 MB (89.03%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    int func(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return nums[0];
+    int robLinear(vector<int>& nums, int start, int end) {
+        int prev2 = 0, prev = 0;
 
-        int prev2 = nums[0];
-        int prev = max(nums[0], nums[1]);
-
-        for(int i=2; i<n; i++) {
+        for(int i=start; i<end; i++) {
             int curr = max(prev, prev2 + nums[i]);
             prev2 = prev;
             prev = curr;
@@ -19,12 +15,9 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-        
-        vector<int> temp1(nums.begin(), nums.end() - 1); // exclude last
-        vector<int> temp2(nums.begin() + 1, nums.end()); // exclude first
 
-        int ans1 = func(temp1);
-        int ans2 = func(temp2);
+        int ans1 = robLinear(nums, 0, n-1);
+        int ans2 = robLinear(nums, 1, n);
 
         return max(ans1, ans2);
     }
