2d37061d85a4077400b6cf46c04a0cfda9d1450f
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-13 18:33:44 +0530

    Time: 0 ms (100%), Space: 9.3 MB (44.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,8 +13,16 @@ public:
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, dp);
-        
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                int up = dp[i-1][j];
+                int left = dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
