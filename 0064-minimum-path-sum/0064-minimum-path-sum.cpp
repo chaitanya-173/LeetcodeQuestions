661c719cdf832ecf80f8bcff4e8cde37ec06776e
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-15 14:01:51 +0530

    Time: 4 ms (15.81%), Space: 15.8 MB (77.09%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp) {
-        if(i == 0 && j == 0) return grid[i][j];
-        if(i < 0 || j < 0) return 1e9;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int left = f(i, j-1, grid, dp);
-        int top = f(i-1, j, grid, dp);
-
-        return dp[i][j] = grid[i][j] + min(left, top);
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n-1, m-1, grid, dp);
+        vector<int> prevRow(m, 0);
+
+        for(int i=0; i<n; i++) {
+            vector<int> currRow(m, 0);
+            for(int j=0; j<m; j++) {
+                if(i == 0 && j == 0) currRow[j] = grid[i][j];
+                else {
+                    int left = 1e9, top = 1e9;
+                    if(j > 0) left = currRow[j-1];
+                    if(i > 0) top = prevRow[j];
+                    currRow[j] = grid[i][j] + min(left, top);
+                }
+            }
+            prevRow = currRow;
+        }
+
+        return prevRow[m-1];
     }
 };
