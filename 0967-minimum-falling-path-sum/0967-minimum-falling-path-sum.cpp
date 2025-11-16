3c9b86e3ddebcf5106541dafb0f88adcea3f2de5
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-16 11:58:58 +0530

    Time: 0 ms (100%), Space: 15 MB (60.88%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -15,24 +15,26 @@ public:
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, 1e9));
+        vector<int> prevRow(n, 1e9);
 
         for(int i=0; i<n; i++) {
+            vector<int> currRow(n, 1e9);
             for(int j=0; j<n; j++) {
-                if(i == 0) dp[i][j] = matrix[i][j];
+                if(i == 0) currRow[j] = matrix[i][j];
                 else {
                     int top = 1e9, topLeft = 1e9, topRight = 1e9;
-                    if(i > 0) top = dp[i-1][j];
-                    if(i > 0 && j > 0) topLeft = dp[i-1][j-1];
-                    if(i > 0 && j+1 < n) topRight = dp[i-1][j+1];
-                    dp[i][j] = matrix[i][j] + min(top, min(topLeft, topRight));
+                    if(i > 0) top = prevRow[j];
+                    if(i > 0 && j > 0) topLeft = prevRow[j-1];
+                    if(i > 0 && j+1 < n) topRight = prevRow[j+1];
+                    currRow[j] = matrix[i][j] + min(top, min(topLeft, topRight));
                 }
             }
+            prevRow = currRow;
         }
 
         int mini = INT_MAX;
         for(int j=0; j<n; j++) {
-            mini = min(mini, dp[n-1][j]);
+            mini = min(mini, prevRow[j]);
         }
         return mini;
     }
