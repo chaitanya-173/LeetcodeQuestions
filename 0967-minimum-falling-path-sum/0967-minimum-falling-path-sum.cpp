0198da5b615e2172243ff79d68770736c004b89d
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-16 19:26:44 +0530

    Time: 4 ms (20.83%), Space: 15 MB (39.4%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-    // int f(int i, int j, int n, vector<vector<int>> &arr, vector<vector<int>> &dp) {
-    //     if(i < 0 || j < 0 || j >= n) return 1e9;
-    //     if(i == 0) return arr[i][j];
-
-    //     if(dp[i][j] != 1e9) return dp[i][j];
-
-    //     int top = f(i-1, j, n, arr, dp);
-    //     int topLeft = f(i-1, j-1, n, arr, dp);
-    //     int topRight = f(i-1, j+1, n, arr, dp);
-
-    //     return dp[i][j] = arr[i][j] + min(top, min(topLeft, topRight));
-    // }
+    int f(int i, int j, int n, vector<vector<int>> &mat, vector<vector<int>> &dp) {
+        // base case
+        if(j < 0 || j >= n) return 1e9;
+        if(i == n-1) return mat[i][j];
+        
+        if(dp[i][j] != 1e9) return dp[i][j];
+        
+        int bottom = f(i+1, j, n, mat, dp);
+        int bottomLeft = f(i+1, j-1, n, mat, dp);
+        int bottomRight = f(i+1, j+1, n, mat, dp);
+        
+        return dp[i][j] = mat[i][j] + min(bottom, min(bottomRight, bottomLeft));
+    }
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<int> prevRow(n, 1e9);
-
-        for(int i=0; i<n; i++) {
-            vector<int> currRow(n, 1e9);
-            for(int j=0; j<n; j++) {
-                if(i == 0) currRow[j] = matrix[i][j];
-                else {
-                    int top = 1e9, topLeft = 1e9, topRight = 1e9;
-                    if(i > 0) top = prevRow[j];
-                    if(i > 0 && j > 0) topLeft = prevRow[j-1];
-                    if(i > 0 && j+1 < n) topRight = prevRow[j+1];
-                    currRow[j] = matrix[i][j] + min(top, min(topLeft, topRight));
-                }
-            }
-            prevRow = currRow;
-        }
-
+        vector<vector<int>> dp(n, vector<int>(n, 1e9));
         int mini = INT_MAX;
         for(int j=0; j<n; j++) {
-            mini = min(mini, prevRow[j]);
+            mini = min(mini, f(0, j, n, matrix, dp));
         }
         return mini;
     }
