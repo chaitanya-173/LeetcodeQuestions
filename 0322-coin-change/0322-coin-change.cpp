26de30857e121525bb17033bf54d6c30589226a1
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-19 22:09:59 +0530

    Time: 51 ms (24.75%), Space: 48.7 MB (43.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int f(int i, int amt, vector<int> &coins, vector<vector<int>> &dp) {
         if(i == 0) {
-            if(amt % coins[i] == 0) return amt / coins[i];
+            if(amt % coins[0] == 0) return amt / coins[0];
             return 1e9;
         }
 
@@ -16,8 +16,20 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = f(n-1, amount, coins, dp);
-        return (ans >= 1e9 ? -1 : ans);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 1e9));
+
+        for(int amt=0; amt<=amount; amt++) {
+            if(amt % coins[0] == 0) dp[0][amt] = amt / coins[0];
+        }
+
+        for(int i=1; i<n; i++) {
+            for(int amt=0; amt<=amount; amt++) {
+                int notPick = dp[i-1][amt];
+                int pick = (coins[i] <= amt ? 1 + dp[i][amt - coins[i]] : 1e9);
+                dp[i][amt] = min(notPick, pick);
+            }
+        }
+
+        return (dp[n-1][amount] >= 1e9 ? -1 : dp[n-1][amount]);
     }
 };
