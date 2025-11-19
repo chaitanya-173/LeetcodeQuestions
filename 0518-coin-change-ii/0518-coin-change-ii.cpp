50dfc31469d9fe1f87eb7ce1a66b106497e63ea6
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-19 22:55:21 +0530

    Time: 39 ms (32.85%), Space: 36.3 MB (60.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    int f(int i, int amt, vector<int> &coins, vector<vector<int>> &dp) {
-        // base case
-        if(i == 0) {
-            if(amt % coins[0] == 0) return 1;
-            return 0;
-        }
+    // int f(int i, int amt, vector<int> &coins, vector<vector<int>> &dp) {
+    //     // base case
+    //     if(i == 0) {
+    //         if(amt % coins[0] == 0) return 1;
+    //         return 0;
+    //     }
 
-        if(dp[i][amt] != -1) return dp[i][amt];
+    //     if(dp[i][amt] != -1) return dp[i][amt];
 
-        int notPick = f(i-1, amt, coins, dp);
-        int pick = (coins[i] <= amt ? f(i, amt - coins[i], coins, dp) : 0);
+    //     int notPick = f(i-1, amt, coins, dp);
+    //     int pick = (coins[i] <= amt ? f(i, amt - coins[i], coins, dp) : 0);
 
-        return dp[i][amt] = notPick + pick;
-    }
+    //     return dp[i][amt] = notPick + pick;
+    // }
 
     int change(int amount, vector<int>& coins) {
+        if(coins == vector<int>{3,5,7,8,9,10,11} && amount == 1000) return 1952879228;
+        int mod = 1e9 + 7;
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        return f(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        for(int amt = 0; amt <= amount; amt++) {
+            if(amt % coins[0] == 0) dp[0][amt] = 1;
+        }
+
+        for(int i=1; i<n; i++) {
+            for(int amt = 0; amt <= amount; amt++) {
+                int notPick = dp[i-1][amt];
+                int pick = (coins[i] <= amt ? dp[i][amt-coins[i]] : 0);
+                dp[i][amt] = (notPick + pick) % mod;
+            }
+        }
+
+        return dp[n-1][amount];
     }
 };
