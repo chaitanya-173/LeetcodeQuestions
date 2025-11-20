fa4c8710d98ea7281ee7764f4c1af18c9daa5127
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-11-20 17:19:27 +0530

    Time: 85 ms (77.04%), Space: 15.6 MB (75.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,24 +6,21 @@ public:
         totalSum = totalSum / 2;
 
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(totalSum + 1, 0));
+        vector<int> prev(totalSum + 1, 0), curr(totalSum + 1, 0);
 
         for(int sum = 0; sum <= totalSum; sum++) {
-            if(nums[0] == sum) dp[0][sum] = 1;
+            if(nums[0] == sum) prev[sum] = 1;
         }
-        
-        // for(int i=0; i<n; i++) {
-        //     dp[i][0] = 0;
-        // }
 
         for(int i=1; i<n; i++) {
             for(int sum = 0; sum <= totalSum; sum++) {
-                int notPick = dp[i-1][sum];
-                int pick = (nums[i] <= sum ? dp[i-1][sum - nums[i]] : 0);
-                dp[i][sum] = notPick || pick;
+                int notPick = prev[sum];
+                int pick = (nums[i] <= sum ? prev[sum - nums[i]] : 0);
+                curr[sum] = notPick || pick;
             }
+            prev = curr;
         }
 
-        return dp[n-1][totalSum];
+        return prev[totalSum];
     }
 };
