39e63fce55ad76ca9ae53ffcf3c5e0f738d99d3f
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-12-06 19:09:50 +0530

    Time: 20 ms (51.99%), Space: 34.8 MB (75.81%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,39 +3,37 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, -1));
+
+        vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
         queue<pair<int,int>> q;
 
-        // Step 1: Push all 0-cells into the queue
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(mat[i][j] == 0) {
-                    dist[i][j] = 0;
                     q.push({i, j});
+                    ans[i][j] = 0;
                 }
             }
         }
 
-        // Directions
-        vector<int> drow = {-1, 0, 1, 0};
-        vector<int> dcol = {0, 1, 0, -1};
+        vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
 
-        // Step 2: Multi-source BFS
         while(!q.empty()) {
             auto [r, c] = q.front();
             q.pop();
 
-            for(int k=0; k<4; k++) {
-                int nr = r + drow[k];
-                int nc = c + dcol[k];
+            for(int i=0; i<4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-                if(nr >= 0 && nc >= 0 && nr < n && nc < m && dist[nr][nc] == -1) {
-                    dist[nr][nc] = dist[r][c] + 1;
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && ans[nr][nc] == INT_MAX) {
+                    ans[nr][nc] = ans[r][c] + 1;
                     q.push({nr, nc});
                 }
             }
         }
 
-        return dist;
+        return ans;
     }
 };
