4ad609bb28e5419a5cc95e0961ccf68ea0c2660c
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-12-06 19:49:48 +0530

    Time: 8 ms (65.45%), Space: 31.6 MB (80.26%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int,int>> q;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if((grid[i][j] == 1) && (i == 0 || j == 0 || i == n-1 || j == m-1)) {
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
+
+        while(!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(int i=0; i<4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && !vis[nr][nc]) {
+                    q.push({nr, nc});
+                    vis[nr][nc] = 1;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(grid[i][j] == 1 && !vis[i][j]) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
