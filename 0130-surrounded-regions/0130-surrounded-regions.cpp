0b53c7f43176147556de1e191301c96eb2591eb4
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-12-07 15:52:03 +0530

    Time: 0 ms (100%), Space: 14.6 MB (35.43%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
+    vector<int> dr = {-1, 0, 1, 0};
+    vector<int> dc = {0, 1, 0, -1};
+
+    void dfs(int i, int j, vector<vector<char>> &board, vector<vector<int>> &vis, int n, int m) {
+        vis[i][j] = 1;
+        for(int k=0; k<4; k++) {
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m  
+                && board[nr][nc] == 'O' && !vis[nr][nc]) {
+                dfs(nr, nc, board, vis, n, m);
+            }
+        }
+    }
+
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<int,int>> q;
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if((board[i][j] == 'O') && (i == 0 || j == 0 || i == n-1 || j == m-1)) {
-                    q.push({i, j});
-                    vis[i][j] = 1;
-                }
-            }
-        }
-
-        vector<int> dr = {-1, 0, 1, 0};
-        vector<int> dc = {0, 1, 0, -1};
-
-        while(!q.empty()) {
-            auto [r, c] = q.front();
-            q.pop();
-
-            for(int i=0; i<4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m  
-                    && board[nr][nc] == 'O' && !vis[nr][nc]) {
-                    q.push({nr, nc});
-                    vis[nr][nc] = 1;
+                    dfs(i, j, board, vis, n, m);
                 }
             }
         }
