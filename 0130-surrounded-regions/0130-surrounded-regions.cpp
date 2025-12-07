3460b895b5ca2962211192f73e3cc8162f7c5b3c
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-12-07 15:54:16 +0530

    Time: 3 ms (25.14%), Space: 14.6 MB (45%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    vector<int> dr = {-1, 0, 1, 0};
-    vector<int> dc = {0, 1, 0, -1};
-
-    void dfs(int i, int j, vector<vector<char>> &board, vector<vector<int>> &vis, int n, int m) {
+    void dfs(int i, int j, vector<vector<char>> &board, vector<vector<int>> &vis, 
+        int n, int m, vector<int> &dr, vector<int> &dc) {
         vis[i][j] = 1;
         for(int k=0; k<4; k++) {
             int nr = i + dr[k];
             int nc = j + dc[k];
             if(nr >= 0 && nr < n && nc >= 0 && nc < m  
                 && board[nr][nc] == 'O' && !vis[nr][nc]) {
-                dfs(nr, nc, board, vis, n, m);
+                dfs(nr, nc, board, vis, n, m, dr, dc);
             }
         }
     }
@@ -21,10 +19,13 @@ public:
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
+        vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
+
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if((board[i][j] == 'O') && (i == 0 || j == 0 || i == n-1 || j == m-1)) {
-                    dfs(i, j, board, vis, n, m);
+                    dfs(i, j, board, vis, n, m, dr, dc);
                 }
             }
         }
