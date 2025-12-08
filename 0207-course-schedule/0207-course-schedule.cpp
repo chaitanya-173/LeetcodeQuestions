88c5dffa6a38f1ac9f71a3476baaf6afee258236
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-12-08 08:38:36 +0530

    Time: 4 ms (66.59%), Space: 20.2 MB (29.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for(int i=0; i<prerequisites.size(); i++) {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj[u].push_back(v);
+        for(auto it: prerequisites) {
+            adj[it[1]].push_back(it[0]);
         }
 
         vector<int> indegree(numCourses, 0);
@@ -27,7 +25,7 @@ public:
             int node = q.front();
             q.pop();
             cnt++;
-            
+
             for(auto it: adj[node]) {
                 indegree[it]--;
                 if(indegree[it] == 0) {
