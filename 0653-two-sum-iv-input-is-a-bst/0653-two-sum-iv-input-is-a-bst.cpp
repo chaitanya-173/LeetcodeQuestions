86bfec1f6b1b33315073c298986cd9b8fc1f5d82
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2025-12-27 21:21:25 +0530

    Time: 0 ms (100%), Space: 37.2 MB (63.77%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,24 +9,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    void findInorder(TreeNode* root, vector<int> &inorder) {
-        if(root == NULL) return;
-        findInorder(root->left, inorder);
-        inorder.push_back(root->val);
-        findInorder(root->right, inorder);
+class BSTIterator {
+private:
+    stack<TreeNode*> st1, st2;
+
+    void pushAllLeft(TreeNode* root) {
+        while(root != NULL) {
+            st1.push(root);
+            root = root->left;
+        }
     }
 
-    bool findTarget(TreeNode* root, int k) {
-        vector<int> inorder;
-        findInorder(root, inorder);
-        int i = 0, j = inorder.size()-1;
-        while(i < j) {
-            if(inorder[i] + inorder[j] < k) i++;
-            else if(inorder[i] + inorder[j] > k) j--;
-            else return true;
+    void pushAllRight(TreeNode* root) {
+        while(root != NULL) {
+            st2.push(root);
+            root = root->right;
         }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushAllLeft(root);
+        pushAllRight(root);
+    }
+
+    int next() {
+        TreeNode* topNode = st1.top();
+        st1.pop();
+        pushAllLeft(topNode->right);
+        return topNode->val;
+    }
+
+    int previous() {
+        TreeNode* topNode = st2.top();
+        st2.pop();
+        pushAllRight(topNode->left);
+        return topNode->val;
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        BSTIterator bst(root);
+        int i = bst.next();
+        int j = bst.previous();
+
+        while(i < j) {
+            if(i + j < k) i = bst.next();
+            else if(i + j > k) j = bst.previous();
+            else return true; 
+        }
+
         return false;
     }
 };
