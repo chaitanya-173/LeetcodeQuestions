5297586979a90c4dccc94aa8b9beafcc2e05be17
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-01-23 19:24:15 +0530

    Time: 0 ms (100%), Space: 9 MB (94.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    void solve( string &digits, string &curr, vector<string> &ans, vector<string> &dict, int i) {
-        if (curr.length() == digits.length()) {
-            ans.push_back(curr);
-            return;
+private:
+    void f(int i, int n, string &digits, vector<string> &dict, string &row, vector<string> &ans) {
+        if(i == n) {
+           ans.push_back(row);
+           return; 
         }
 
         int digit = digits[i] - '0';
         string temp = dict[digit];
-        for (int j = 0; j < temp.length(); j++) {
-            curr.push_back(temp[j]);
-            solve(digits, curr, ans, dict, i+1);
-            curr.pop_back();
+        for(int j=0; j<temp.size(); j++) {
+            row.push_back(temp[j]);
+            f(i+1, n, digits, dict, row, ans);
+            row.pop_back();
         }
     }
 
 public:
-    vector<string> letterCombinations(string digits) { 
+    vector<string> letterCombinations(string digits) {
+        int n = digits.size();
         vector<string> dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        string curr;
         vector<string> ans;
-
-        solve( digits, curr, ans, dict, 0 );
-
+        string row = "";
+        f(0, n, digits, dict, row, ans);
         return ans;
     }
 };
