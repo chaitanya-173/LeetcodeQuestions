26d72a53c48c3822d69c207810af764163e16592
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-01-30 23:33:15 +0530

    Time: 22 ms (62.92%), Space: 139.3 MB (25.44%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;   // store indices
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++) {
+
+            // window se bahar wala index hatao
+            if(!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+                
+            // chhote elements hatao (back se)
+            while(!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            // current index add karo
+            dq.push_back(i);
+
+            // answer tab push karo jab window ban jaaye
+            if(i >= k - 1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
