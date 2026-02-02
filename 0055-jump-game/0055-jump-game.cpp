9bf857163f5b1cd460b5e0ed8b1f370056299d7c
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-02 20:09:50 +0530

    Time: 590 ms (8.2%), Space: 55.6 MB (7.55%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    bool f(int ind, vector<int> &arr, vector<int> &dp) {
+        if(ind == arr.size()-1) return true;
+        if(arr[ind] == 0) return false;
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int num = arr[ind];
+        for(int i=1; i<=num; i++) {
+            if(f(ind+i, arr, dp)) return dp[ind] = true;
+        } 
+        return dp[ind] = false;
+    }
+
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return f(0, nums, dp);
+    }
+};
