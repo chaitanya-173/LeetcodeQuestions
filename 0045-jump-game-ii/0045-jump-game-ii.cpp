9e1e1a03a4e5559bea97871f1731fe5ad5d567fc
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-02 20:38:51 +0530

    Time: 0 ms (100%), Space: 20.3 MB (97.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,21 @@
 class Solution {
-private:
-    int f(int ind, vector<int> &arr, vector<int> &dp, int n) {
-        if(ind >= n-1) return 0;
-        if(arr[ind] == 0) return 1e9;
-
-        if(dp[ind] != -1) return dp[ind];
-
-        int ans = 1e9;
-        for(int i = 1; i <= arr[ind]; i++) {
-            if(ind + i < n) {
-                ans = min(ans, 1 + f(ind + i, arr, dp, n));
-            }
-        }
-        return dp[ind] = ans;
-    }
-
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return f(0, nums, dp, n);
+        int jumps = 0;
+
+        int left = 0, right = 0;
+
+        while(right < n - 1) {
+            int farthest = 0;
+            for(int i = left; i <= right; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            left = right + 1;
+            right = farthest; 
+            jumps++;
+        }
+
+        return jumps;
     }
 };
