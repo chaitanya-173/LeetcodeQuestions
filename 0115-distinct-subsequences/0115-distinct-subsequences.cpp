c1d8d0702b2f97ba8b668a550528a9f4ba560546
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-03 22:07:10 +0530

    Time: 39 ms (20.05%), Space: 27 MB (63.14%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,20 @@
 class Solution {
+private:
+    int f(int i, int j, string &s, string &t, int n, int m, vector<vector<int>> &dp) {
+        if(j == m) return 1;
+        if(i == n) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == t[j]) return dp[i][j] = f(i+1, j, s, t, n, m, dp) + f(i+1, j+1, s, t, n, m, dp);
+        return dp[i][j] = f(i+1, j, s, t, n, m, dp);
+    }
+
 public:
     int numDistinct(string s, string t) {
-        int m = s.length();
-        int n = t.length();
-
-        vector<vector<unsigned long long>> dp(
-            m + 1, vector<unsigned long long>(n + 1, 0));
-
-        for (int i = 0; i <= m; i++) {
-            dp[i][n] = 1;
-        }
-
-        for (int sIndex = m - 1; sIndex >= 0; sIndex--) {
-            for (int tIndex = n - 1; tIndex >= 0; tIndex--) {
-                dp[sIndex][tIndex] = dp[sIndex + 1][tIndex];
-
-                if (s[sIndex] == t[tIndex]) {
-                    dp[sIndex][tIndex] += dp[sIndex + 1][tIndex + 1];
-                }
-            }
-        }
-
-        return dp[0][0];
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(0, 0, s, t, n, m, dp);
     }
 };
