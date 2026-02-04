f7f318616877b6b7c0caa20a40716f3415c892c8
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-04 15:34:12 +0530

    Time: 43 ms (27.52%), Space: 61.8 MB (6.58%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -14,8 +14,20 @@ private:
 
 public:
     int change(int amount, vector<int>& coins) {
+        using ull = unsigned long long;
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return f(0, amount, coins, n, dp);
+        vector<vector<ull>> dp(n+1, vector<ull>(amount+1, 0));
+
+        for(int i=0; i<=n; i++) dp[i][0] = 1;
+        for(int i=n-1; i>=0; i--) {
+            for(int amt = 0; amt <= amount; amt++) {
+                ull notPick = dp[i+1][amt];
+                ull pick = (coins[i] <= amt ? dp[i][amt - coins[i]] : 0);
+
+                dp[i][amt] = notPick + pick;
+            }
+        }
+
+        return dp[0][amount];
     }
 };
