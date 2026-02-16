2feab76fb4afa2679aba01a8a91c6f98fd3848fa
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-16 19:48:51 +0530

    Time: 44 ms (28.37%), Space: 32.4 MB (36.71%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-   bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
-        int next = hand[i] + 1;
-        hand[i] = -1;  // Mark as used
-        int count = 1;
-        i += 1;
-        while (i < n && count < groupSize) {
-            if (hand[i] == next) {
-                next = hand[i] + 1;
-                hand[i] = -1;
-                count++;
-            }
-            i++;
-        }
-        return count == groupSize;
-    }
-
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n = hand.size();
-        if (n % groupSize != 0) return false;
-        std::sort(hand.begin(), hand.end());
-        for (int i = 0; i < n; i++) {
-            if (hand[i] >= 0) {
-                if (!findSuccessors(hand, groupSize, i, n)) return false;
+        if(n % groupSize != 0) return false;
+
+        map<int,int> freq;
+        for(auto it: hand) freq[it]++;
+        sort(hand.begin(), hand.end());
+
+        while(!freq.empty()) {
+            int cur = freq.begin()->first;
+
+            for(int i=0; i<groupSize; i++) {
+                if(!freq[cur + i]) return false;
+                freq[cur + i]--;
+                if(freq[cur + i] == 0) freq.erase(cur + i);
             }
         }
+
         return true;
     }
 };
