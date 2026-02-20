2de9254d05dd9a60a52854af55fd0ac20c7a01ae
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-20 12:41:38 +0530

    Time: 5 ms (7.06%), Space: 16.3 MB (41.54%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
+    // int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp) {
+    //     if(i == 0 && j == 0) return grid[i][j];
+    //     if(i < 0 || j < 0) return 1e9;
+
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     int left = f(i, j-1, grid, dp);
+    //     int top = f(i-1, j, grid, dp);
+
+    //     return dp[i][j] = grid[i][j] + min(left, top);
+    // }
+
+    int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp, int n, int m) {
+        if(i == n-1 && j == m-1) return grid[i][j];
+        if(i >= n || j >= m) return 1e9; 
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int right = f(i, j+1, grid, dp, n, m);
+        int bottom = f(i+1, j, grid, dp, n, m);
+
+        return dp[i][j] = grid[i][j] + min(right, bottom);
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<int> prevRow(m, 0);
-
-        for(int i=0; i<n; i++) {
-            vector<int> currRow(m, 0);
-            for(int j=0; j<m; j++) {
-                if(i == 0 && j == 0) currRow[j] = grid[i][j];
-                else {
-                    int left = 1e9, top = 1e9;
-                    if(j > 0) left = currRow[j-1];
-                    if(i > 0) top = prevRow[j];
-                    currRow[j] = grid[i][j] + min(left, top);
-                }
-            }
-            prevRow = currRow;
-        }
-
-        return prevRow[m-1];
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(0, 0, grid, dp, n, m);
     }
 };
