90a75988c1e67694cbef3f65956d908cc5867c4d
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-21 13:26:04 +0530

    Time: 0 ms (100%), Space: 20.5 MB (82.67%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for(int i=0; i<nums.size(); i++) {
-            ans = ans ^ nums[i];
-        }
+        for(auto it: nums) ans = ans ^ it;
         return ans;
     }
 };
