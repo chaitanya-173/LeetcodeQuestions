5963dbc0cd1c17fb0bf49c0b8558fe2273fb9fa0
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-21 12:12:34 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.19%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n);
+        int cnt = 0;
+        while(n) {
+            n = (n & n-1);
+            cnt++;
+        }
+        return cnt;
     }
 };
