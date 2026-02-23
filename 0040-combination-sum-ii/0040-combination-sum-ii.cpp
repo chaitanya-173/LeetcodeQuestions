8a2f7672fa59c4007a7540ab17199832f5621191
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-02-23 22:49:42 +0530

    Time: 1 ms (51.05%), Space: 14.1 MB (37.9%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,27 @@
 class Solution {
 private:
-    void f(int idx, int target, vector<int> &candidates,
-           vector<int> &path, vector<vector<int>> &ans) {
+    void solve(int ind, int target, vector<int> &arr, vector<int> &temp, vector<vector<int>> &ans) {
         if(target == 0) {
-            ans.push_back(path);
+            ans.push_back(temp);
             return;
         }
 
-        for(int i = idx; i < candidates.size(); i++) {
-            if(i > idx && candidates[i] == candidates[i-1]) continue; // skip duplicates
-            if(candidates[i] > target) break;
+        for(int i=ind; i<arr.size(); i++) {
+            if(i > ind && arr[i] == arr[i-1]) continue;
+            if(arr[i] > target) break;
 
-            path.push_back(candidates[i]);
-            f(i + 1, target - candidates[i], candidates, path, ans);
-            path.pop_back();
+            temp.push_back(arr[i]);
+            solve(i+1, target-arr[i], arr, temp, ans);
+            temp.pop_back();
         }
     }
 
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
-        vector<int> path;
-        f(0, target, candidates, path, ans);
+        vector<int> temp;
+        sort(candidates.begin(), candidates.end());
+        solve(0, target, candidates, temp, ans);
         return ans;
     }
 };
