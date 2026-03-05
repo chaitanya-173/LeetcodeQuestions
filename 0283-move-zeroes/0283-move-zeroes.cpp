89518c46c9813cf71cc4bc12ec7d86674491bc80
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-03-05 09:41:04 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int j = -1;
-        for(int i=0; i<n; i++) {
-            if(nums[i] == 0) {
-                j = i;
-                break;
-            }
+        int i = 0;
+        for(i = 0; i < nums.size(); i++) {
+            if(nums[i] == 0) break;
         }
 
-        if(j == -1) return;
-
-        for(int i=j+1; i<n; i++) {
-            if(nums[i] != 0) {
+        for(int j=i+1; j<nums.size(); j++) {
+            if(nums[j] != 0) {
                 swap(nums[i], nums[j]);
-                j++;
+                i++;
             }
-        }
+        }  
     }
 };
