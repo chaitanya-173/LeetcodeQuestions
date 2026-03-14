36af6c36bce278864f88d1f2dd8155475b1a29e4
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-03-14 20:51:18 +0530

    Time: 39 ms (94.96%), Space: 29 MB (71.89%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
+
         for(int i=0; i<n; i++) {
             if(i>0 && nums[i] == nums[i-1]) continue;
             int j = i+1;
@@ -15,15 +16,15 @@ public:
                 } else if(sum > 0) {
                     k--;
                 } else {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    ans.push_back(temp);
+                    ans.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                    while(j < k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
+
         return ans;
     }
 };
