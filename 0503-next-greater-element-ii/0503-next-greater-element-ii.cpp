e1ec4317eb50ecf6e4f0719d1063c11b59f31d4e
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-03-17 12:57:45 +0530

    Time: 1 ms (82.55%), Space: 28.6 MB (20.45%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,20 +4,18 @@ public:
         int n = nums.size();
         vector<int> ans(n, -1);
         stack<int> st;
-        
+
         for(int i=2*n-1; i>=0; i--) {
-            
-            while(!st.empty() && st.top() <= nums[i % n]) {
-                st.pop();
-            }
-            
+            while(!st.empty() && st.top() <= nums[i % n]) st.pop();
+
             if(i < n) {
-                ans[i] = (st.empty() ? -1 : st.top());
+                if(st.empty()) ans[i] = -1;
+                else ans[i] = st.top();
             }
-            
+
             st.push(nums[i % n]);
         }
-        
+
         return ans;
     }
 };
