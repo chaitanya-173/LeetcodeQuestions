09422362d6b2cb79241a1ec1cddbf1b3b78fda05
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-04-03 21:46:30 +0530

    Time: 48 ms (9.73%), Space: 17.6 MB (36.04%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -2,35 +2,26 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
+        vector<int> dp(n, 1), cnt(n, 1);
+        int maxi = 1;
 
-        vector<int> lengths(n, 1); 
-        vector<int> counts(n, 1); 
-        vector<int> bit(n + 1, 0); 
-
-        int max_length = 1; 
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    if (lengths[j] + 1 > lengths[i]) {
-                        lengths[i] = lengths[j] + 1;
-                        counts[i] = counts[j];
-                    } else if (lengths[j] + 1 == lengths[i]) {
-                        counts[i] += counts[j];
-                    }
+        for(int i=0; i<n; i++) {
+            for(int prev=0; prev<i; prev++) {
+                if(nums[prev] < nums[i] && 1 + dp[prev] > dp[i]) {
+                    dp[i] = 1 + dp[prev];
+                    cnt[i] = cnt[prev];
+                } else if(nums[prev] < nums[i] && 1 + dp[prev] == dp[i]) {
+                    cnt[i] += cnt[prev];
                 }
             }
-            max_length = max(max_length, lengths[i]);
+            maxi = max(maxi, dp[i]);
         }
 
-        int result = 0;
-        for (int i = 0; i < n; ++i) {
-            if (lengths[i] == max_length) {
-                result += counts[i];
-            }
+        int nos = 0;
+        for(int i=0; i<n; i++) {
+            if(dp[i] == maxi) nos += cnt[i];
         }
 
-        return result;
+        return nos;
     }
 };
