325feb35c3fa105f0faf824aeb1e16f75b0a60fa
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-04-12 08:59:32 +0530

    Time: 55 ms (28.92%), Space: 48.6 MB (21.29%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,46 +1,30 @@
 class Solution {
-private:
-    int findDist(char ind1, char ind2, unordered_map<char,pair<int,int>> &hash) {
-        auto [x1, y1] = hash[ind1];
-        auto [x2, y2] = hash[ind2];
+public:
+    int dist(int a, int b) {
+        if(a == -1) return 0;
+        int x1 = a / 6, y1 = a % 6;
+        int x2 = b / 6, y2 = b % 6;
         return abs(x1 - x2) + abs(y1 - y2);
     }
 
-    int f(int i, int f1, int f2, string &word,
-          unordered_map<char,pair<int,int>> &hash,
-          vector<vector<vector<int>>> &dp) {
-
+    int f(int i, int f1, int f2, string &word, vector<vector<vector<int>>> &dp) {
         if(i == word.size()) return 0;
 
         if(dp[i][f1+1][f2+1] != -1) return dp[i][f1+1][f2+1];
 
-        int curr = word[i];
+        int curr = word[i] - 'A';
 
-        int d1 = (f1 == -1) ? 0 : findDist(f1 + 'A', curr, hash);
-        int useF1 = d1 + f(i + 1, curr - 'A', f2, word, hash, dp);
+        int op1 = dist(f1, curr) + f(i+1, curr, f2, word, dp); // use f1
+        int op2 = dist(f2, curr) + f(i+1, f1, curr, word, dp); // use f2
 
-        int d2 = (f2 == -1) ? 0 : findDist(f2 + 'A', curr, hash);
-        int useF2 = d2 + f(i + 1, f1, curr - 'A', word, hash, dp);
-
-        return dp[i][f1+1][f2+1] = min(useF1, useF2);
+        return dp[i][f1+1][f2+1] = min(op1, op2);
     }
 
-public:
     int minimumDistance(string word) {
-        unordered_map<char, pair<int,int>> hash;
-
-        char ch = 'A';
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 6; j++) {
-                hash[ch] = {i, j};
-                if(ch == 'Z') break;
-                ch++;
-            }
-        }
-
         int n = word.size();
+
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(27, vector<int>(27, -1)));
 
-        return f(0, -1, -1, word, hash, dp);
+        return f(0, -1, -1, word, dp);
     }
 };
