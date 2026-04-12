14070acb1b6226f142ebf31d00b31a85a6b90472
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-04-12 09:23:50 +0530

    Time: 43 ms (35.34%), Space: 9.4 MB (75.9%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -7,24 +7,36 @@ public:
         return abs(x1 - x2) + abs(y1 - y2);
     }
 
-    int f(int i, int f1, int f2, string &word, vector<vector<vector<int>>> &dp) {
-        if(i == word.size()) return 0;
+    // int f(int i, int f1, int f2, string &word, vector<vector<vector<int>>> &dp) {
+    //     if(i == word.size()) return 0;
 
-        if(dp[i][f1+1][f2+1] != -1) return dp[i][f1+1][f2+1];
+    //     if(dp[i][f1+1][f2+1] != -1) return dp[i][f1+1][f2+1];
 
-        int curr = word[i] - 'A';
+    //     int cur = word[i] - 'A';
 
-        int op1 = dist(f1, curr) + f(i+1, curr, f2, word, dp); // use f1
-        int op2 = dist(f2, curr) + f(i+1, f1, curr, word, dp); // use f2
+    //     int op1 = dist(f1, cur) + f(i+1, cur, f2, word, dp); // use f1
+    //     int op2 = dist(f2, cur) + f(i+1, f1, cur, word, dp); // use f2
 
-        return dp[i][f1+1][f2+1] = min(op1, op2);
-    }
+    //     return dp[i][f1+1][f2+1] = min(op1, op2);
+    // }
 
     int minimumDistance(string word) {
         int n = word.size();
+        vector<vector<int>> next(27, vector<int>(27, 0));
+        vector<vector<int>> cur(27, vector<int>(27, 0));
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(27, vector<int>(27, -1)));
-
-        return f(0, -1, -1, word, dp);
+        for(int i = n-1; i >= 0; i--) {
+            int c = word[i] - 'A';
+            for(int f1 = 25; f1 >= -1; f1--) {
+                for(int f2 = 25; f2 >= -1; f2--) {
+                    int op1 = dist(f1, c) + next[c+1][f2+1];
+                    int op2 = dist(f2, c) + next[f1+1][c+1];
+                    cur[f1+1][f2+1] = min(op1, op2);
+                }
+            }
+            next = cur;
+        }
+        
+        return next[0][0];
     }
 };
