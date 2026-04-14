94e98936753bd437ce769952a5fd57783d58099f
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-04-14 09:00:11 +0530

    Time: 31 ms (61.64%), Space: 14.5 MB (56.16%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -2,17 +2,17 @@ class Solution {
 private:
     using ll = long long;
 
-    ll f(int i, int j, vector<int> &robot, vector<int> &slots, vector<vector<ll>> &dp) {
-        if(i == robot.size()) return 0;
-        if(j == slots.size()) return 1e15;
+    // ll f(int i, int j, vector<int> &robot, vector<int> &slots, vector<vector<ll>> &dp) {
+    //     if(i == robot.size()) return 0;
+    //     if(j == slots.size()) return 1e15;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        ll take = abs(robot[i] - slots[j]) + f(i+1, j+1, robot, slots, dp);
-        ll notTake = f(i, j+1, robot, slots, dp);
+    //     ll take = abs(robot[i] - slots[j]) + f(i+1, j+1, robot, slots, dp);
+    //     ll notTake = f(i, j+1, robot, slots, dp);
 
-        return dp[i][j] = min(take, notTake);
-    }
+    //     return dp[i][j] = min(take, notTake);
+    // }
 
 public:
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
@@ -27,8 +27,21 @@ public:
         }
 
         int n = robot.size(), m = slots.size(); 
-        vector<vector<ll>> dp(n, vector<ll>(m, -1));
+        // vector<vector<ll>> dp(n+1, vector<ll>(m+1, 0));
+        vector<ll> cur(m+1, 0), next(m+1, 0);
+        
+        // base case 
+        cur[m] = 1e15;
 
-        return f(0, 0, robot, slots, dp);
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                ll take = abs(robot[i] - slots[j]) + next[j+1];
+                ll notTake = cur[j+1];
+                cur[j] = min(take, notTake);
+            }
+            next = cur;
+        }
+
+        return next[0];
     }
 };
