f49a938d8328d7eaa704c47a778f04ce447b6b52
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-04-26 10:53:41 +0530

    Time: 27 ms (63.64%), Space: 71.2 MB (48.18%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,4 +1,27 @@
 class Solution {
+    vector<int> dr = {-1, 0, 1, 0};
+    vector<int> dc = {0, 1, 0, -1};
+
+    bool dfs(int r, int c, int parr, int parc, vector<vector<char>> &grid, vector<vector<int>> &vis) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vis[r][c] = 1;
+        for(int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == grid[r][c]) {
+                if(vis[nr][nc] && !(nr == parr && nc == parc)) return true;
+                else if(!vis[nr][nc]) {
+                    if(dfs(nr, nc, r, c, grid, vis)) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 public:
     bool containsCycle(vector<vector<char>>& grid) {
         int n = grid.size();
@@ -6,35 +29,10 @@ public:
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        vector<int> dr = {-1, 0, 1, 0};
-        vector<int> dc = {0, 1, 0, -1};
-
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(!vis[i][j]) {
-
-                    queue<tuple<int,int,int,int>> q;
-                    q.push({i, j, -1, -1});
-                    vis[i][j] = 1;
-
-                    while(!q.empty()) {
-                        auto [r, c, parr, parc] = q.front();
-                        q.pop();
-
-                        for(int k = 0; k < 4; k++) {
-                            int nr = r + dr[k];
-                            int nc = c + dc[k];
-                            
-                            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == grid[r][c]) {
-                                if(vis[nr][nc] && !(nr == parr && nc == parc)) return true;
-                                else if(!vis[nr][nc]) {
-                                    q.push({nr, nc, r, c});
-                                    vis[nr][nc] = 1;
-                                }
-                            }
-                        }
-                    }
-
+                    if(dfs(i, j, -1, -1, grid, vis)) return true;
                 }
             }
         }
