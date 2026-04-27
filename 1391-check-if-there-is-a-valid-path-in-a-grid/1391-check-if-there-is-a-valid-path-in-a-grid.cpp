713e325a94beb6fa8ab7ebc498f10a25ab9d234c
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-04-27 18:57:34 +0530

    Time: 14 ms (86.29%), Space: 62.7 MB (38.71%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -7,78 +7,42 @@ public:
         queue<pair<int,int>> q;
         vector<vector<int>> vis(n, vector<int>(m, 0));
         q.push({0, 0});
-        vis[0][0] = 1;
 
         while(!q.empty()) {
             auto [r, c] = q.front();
             q.pop();
+            vis[r][c] = 1;
             if(r == n-1 && c == m-1) return true;
 
             if(grid[r][c] == 1) {
 
-                if(c-1 >= 0 && !vis[r][c-1] && (grid[r][c-1] == 1 || grid[r][c-1] == 4 || grid[r][c-1] == 6)) {
-                    q.push({r, c-1}); // left
-                    vis[r][c-1] = 1;
-                }
-                if(c+1 < m && !vis[r][c+1] && (grid[r][c+1] == 1 || grid[r][c+1] == 3 || grid[r][c+1] == 5)) {
-                    q.push({r, c+1}); // right 
-                    vis[r][c+1] = 1;
-                }         
+                if(c-1 >= 0 && !vis[r][c-1] && (grid[r][c-1] == 1 || grid[r][c-1] == 4 || grid[r][c-1] == 6)) q.push({r, c-1}); // left
+                if(c+1 < m && !vis[r][c+1] && (grid[r][c+1] == 1 || grid[r][c+1] == 3 || grid[r][c+1] == 5)) q.push({r, c+1}); // right          
 
             } else if(grid[r][c] == 2) {
 
-                if(r-1 >= 0 && !vis[r-1][c] && (grid[r-1][c] == 2 || grid[r-1][c] == 3 || grid[r-1][c] == 4)) {
-                    q.push({r-1, c}); // top
-                    vis[r-1][c] = 1;
-                }
-                if(r+1 < n && !vis[r+1][c] && (grid[r+1][c] == 2 || grid[r+1][c] == 5 || grid[r+1][c] == 6)) {
-                    q.push({r+1, c}); // bottom
-                    vis[r+1][c] = 1;
-                }
+                if(r-1 >= 0 && !vis[r-1][c] && (grid[r-1][c] == 2 || grid[r-1][c] == 3 || grid[r-1][c] == 4)) q.push({r-1, c}); // top
+                if(r+1 < n && !vis[r+1][c] && (grid[r+1][c] == 2 || grid[r+1][c] == 5 || grid[r+1][c] == 6)) q.push({r+1, c}); // bottom
 
             } else if(grid[r][c] == 3) {
 
-                if(c-1 >= 0 && !vis[r][c-1] && (grid[r][c-1] == 1 || grid[r][c-1] == 4 || grid[r][c-1] == 6)) {
-                    q.push({r, c-1}); // left
-                    vis[r][c-1] = 1;
-                }
-                if(r+1 < n && !vis[r+1][c] && (grid[r+1][c] == 2 || grid[r+1][c] == 5 || grid[r+1][c] == 6)) {
-                    q.push({r+1, c}); // bottom
-                    vis[r+1][c] = 1;
-                }
+                if(c-1 >= 0 && !vis[r][c-1] && (grid[r][c-1] == 1 || grid[r][c-1] == 4 || grid[r][c-1] == 6)) q.push({r, c-1}); // left
+                if(r+1 < n && !vis[r+1][c] && (grid[r+1][c] == 2 || grid[r+1][c] == 5 || grid[r+1][c] == 6)) q.push({r+1, c}); // bottom
 
             } else if(grid[r][c] == 4) {
 
-                if(c+1 < m && !vis[r][c+1] && (grid[r][c+1] == 1 || grid[r][c+1] == 3 || grid[r][c+1] == 5)) {
-                    q.push({r, c+1}); // right 
-                    vis[r][c+1] = 1;
-                }
-                if(r+1 < n && !vis[r+1][c] && (grid[r+1][c] == 2 || grid[r+1][c] == 5 || grid[r+1][c] == 6)) {
-                    q.push({r+1, c}); // bottom
-                    vis[r+1][c] = 1;
-                }
+                if(c+1 < m && !vis[r][c+1] && (grid[r][c+1] == 1 || grid[r][c+1] == 3 || grid[r][c+1] == 5)) q.push({r, c+1}); // right 
+                if(r+1 < n && !vis[r+1][c] && (grid[r+1][c] == 2 || grid[r+1][c] == 5 || grid[r+1][c] == 6)) q.push({r+1, c}); // bottom
 
             } else if(grid[r][c] == 5) {
 
-                if(c-1 >= 0 && !vis[r][c-1] && (grid[r][c-1] == 1 || grid[r][c-1] == 4 || grid[r][c-1] == 6)) {
-                    q.push({r, c-1}); // left
-                    vis[r][c-1] = 1;
-                }
-                if(r-1 >= 0 && !vis[r-1][c] && (grid[r-1][c] == 2 || grid[r-1][c] == 3 || grid[r-1][c] == 4)) {
-                    q.push({r-1, c}); // top
-                    vis[r-1][c] = 1;
-                }
+                if(c-1 >= 0 && !vis[r][c-1] && (grid[r][c-1] == 1 || grid[r][c-1] == 4 || grid[r][c-1] == 6)) q.push({r, c-1}); // left
+                if(r-1 >= 0 && !vis[r-1][c] && (grid[r-1][c] == 2 || grid[r-1][c] == 3 || grid[r-1][c] == 4)) q.push({r-1, c}); // top
 
             } else {
 
-                if(r-1 >= 0 && !vis[r-1][c] && (grid[r-1][c] == 2 || grid[r-1][c] == 3 || grid[r-1][c] == 4)) {
-                    q.push({r-1, c}); // top
-                    vis[r-1][c] = 1;
-                }
-                if(c+1 < m && !vis[r][c+1] && (grid[r][c+1] == 1 || grid[r][c+1] == 3 || grid[r][c+1] == 5)) {
-                    q.push({r, c+1}); // right 
-                    vis[r][c+1] = 1;
-                }
+                if(r-1 >= 0 && !vis[r-1][c] && (grid[r-1][c] == 2 || grid[r-1][c] == 3 || grid[r-1][c] == 4)) q.push({r-1, c}); // top
+                if(c+1 < m && !vis[r][c+1] && (grid[r][c+1] == 1 || grid[r][c+1] == 3 || grid[r][c+1] == 5)) q.push({r, c+1}); // right 
 
             }
         }
