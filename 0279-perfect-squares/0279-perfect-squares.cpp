2e08cd762016f030c11d537de23f7ce451a35389
Chaitanya Chaudhary <chaitanyachaudhary73@gmail.com>
2026-05-04 08:59:06 +0530

    Time: 43 ms (68.04%), Space: 13.1 MB (80.77%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int solve(int n, vector<int> &dp) {
-        // Base case:
-        // Agar n = 0 ho gaya, matlab koi number bachha hi nahi,
-        // toh aur squares ki zarurat nahi
-        if (n == 0) return 0;
+        if(n == 0) return 0; // BC: no need of mroe squares if n=0
 
         if(dp[n] != -1) return dp[n];
 
-        // Minimum answer track karne ke liye
-        int ans = INT_MAX;
+        int ans = n; // to track min. ans
 
-        // Har possible perfect square try karo
-        // Example: n = 12
-        // squares: 1, 4, 9
-        for (int i = 1; i * i <= n; i++) {
+        // try every possible perfect square
+        for(int i = 1; i * i <= n; i++) {
             int sq = i * i;
 
-            // Current square use kiya (+1)
-            // Remaining value ke liye recursion
+            // recursion for rem. values
             ans = min(ans, 1 + solve(n - sq, dp));
         }
 
         return dp[n] = ans;
-    }
+    } 
 
     int numSquares(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return solve(n, dp);
+
+        vector<int> dp(n+1, n);
+        dp[0] = 0;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                int sq = j * j;
+                dp[i] = min(dp[i], 1 + dp[i - sq]);
+            }
+        }
+
+        return dp[n];
     }
 };
